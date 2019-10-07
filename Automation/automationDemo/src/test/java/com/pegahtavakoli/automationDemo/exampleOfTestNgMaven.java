a0c7002c298e59d4e13f34c8b6bf4d55a72cc2c6
pegahtav <31268042+pegahtav@users.noreply.github.com>
2019-10-07 15:31:38 -0700

    Add files via upload

---
+++ Automation/automationDemo/src/test/java/com/pegahtavakoli/automationDemo/exampleOfTestNgMaven.java
@@ -0,0 +1,10 @@
+package com.pegahtavakoli.automationDemo;
+
+import org.testng.annotations.Test;
+
+public class exampleOfTestNgMaven {
+    @Test
+    public void exampleOfTestNgMaven() {
+        System.out.println("This is TestNG-Maven Example");
+    }
+}
